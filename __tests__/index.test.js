b859b96c99c623d8049ccf44b7b2d22d9fd7aa41
Daniel Bayerlein <daniel.bayerlein@googlemail.com>
2017-04-07 16:35:23 +0200

    Use JavaScript Standard Style (https://standardjs.com)

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,65 +1,65 @@
-jest.mock('os');
-const os = require('os');
-const index = require('../src/');
+jest.mock('os')
+const os = require('os')
+const index = require('../src/')
 
 describe('index.js', () => {
   test('returns an Promise', () => {
-    expect(index()()).toBeInstanceOf(Promise);
-  });
+    expect(index()()).toBeInstanceOf(Promise)
+  })
 
   test('returns an object with a id', () => (
     index()().then((result) => {
-      expect(result[0].id).toBeDefined();
+      expect(result[0].id).toBeDefined()
     })
-  ));
+  ))
 
   test('returns an object with a title', () => (
     index()().then((result) => {
-      expect(result[0].title).toBeDefined();
+      expect(result[0].title).toBeDefined()
     })
-  ));
+  ))
 
   test('returns the system uptime (full)', () => {
-    os.uptime = jest.fn(() => 99368);
+    os.uptime = jest.fn(() => 99368)
 
     return index()().then((result) => {
       expect(result).toEqual([{
         id: 'zazu-uptime.99368',
-        title: '01d 03h 36m 08s',
-      }]);
-    });
-  });
+        title: '01d 03h 36m 08s'
+      }])
+    })
+  })
 
   test('returns the system uptime (hours)', () => {
-    os.uptime = jest.fn(() => 9368);
+    os.uptime = jest.fn(() => 9368)
 
     return index()().then((result) => {
       expect(result).toEqual([{
         id: 'zazu-uptime.9368',
-        title: '02h 36m 08s',
-      }]);
-    });
-  });
+        title: '02h 36m 08s'
+      }])
+    })
+  })
 
   test('returns the system uptime (minutes)', () => {
-    os.uptime = jest.fn(() => 368);
+    os.uptime = jest.fn(() => 368)
 
     return index()().then((result) => {
       expect(result).toEqual([{
         id: 'zazu-uptime.368',
-        title: '06m 08s',
-      }]);
-    });
-  });
+        title: '06m 08s'
+      }])
+    })
+  })
 
   test('returns the system uptime (seconds)', () => {
-    os.uptime = jest.fn(() => 36);
+    os.uptime = jest.fn(() => 36)
 
     return index()().then((result) => {
       expect(result).toEqual([{
         id: 'zazu-uptime.36',
-        title: '36s',
-      }]);
-    });
-  });
-});
+        title: '36s'
+      }])
+    })
+  })
+})
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const os = require('os');
+const os = require('os')
 
 /**
  * Pad a number to two digits
@@ -6,14 +6,14 @@ const os = require('os');
  * @param  {number} number Number to pad
  * @return {string}        Padded number
  */
-const pad2 = number => `${number < 10 ? 0 : ''}${number}`;
+const pad2 = (number) => `${number < 10 ? 0 : ''}${number}`
 
 /**
  * Returns the system uptime in number of seconds
  *
  * @return {number} System uptime
  */
-const getUptime = () => os.uptime();
+const getUptime = () => os.uptime()
 
 /**
  * Returns the formatted system uptime
@@ -22,33 +22,33 @@ const getUptime = () => os.uptime();
  * @return {string} Uptime
  */
 const format = (uptime) => {
-  const date = new Date(uptime * 1000);
+  const date = new Date(uptime * 1000)
 
-  const days = pad2(parseInt(uptime / 86400, 10));
-  const hours = pad2(date.getUTCHours());
-  const minutes = pad2(date.getUTCMinutes());
-  const seconds = pad2(date.getUTCSeconds());
+  const days = pad2(parseInt(uptime / 86400, 10))
+  const hours = pad2(date.getUTCHours())
+  const minutes = pad2(date.getUTCMinutes())
+  const seconds = pad2(date.getUTCSeconds())
 
   if (days === '00' && hours === '00' && minutes === '00') {
-    return `${seconds}s`;
+    return `${seconds}s`
   }
 
   if (days === '00' && hours === '00') {
-    return `${minutes}m ${seconds}s`;
+    return `${minutes}m ${seconds}s`
   }
 
   if (days === '00') {
-    return `${hours}h ${minutes}m ${seconds}s`;
+    return `${hours}h ${minutes}m ${seconds}s`
   }
 
-  return `${days}d ${hours}h ${minutes}m ${seconds}s`;
-};
+  return `${days}d ${hours}h ${minutes}m ${seconds}s`
+}
 
 module.exports = () => () => {
-  const uptime = getUptime();
+  const uptime = getUptime()
 
-  return new Promise(resolve => resolve([{
+  return new Promise((resolve) => resolve([{
     id: `zazu-uptime.${uptime}`,
-    title: format(uptime),
-  }]));
-};
+    title: format(uptime)
+  }]))
+}
